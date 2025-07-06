1a68931d8a7d729fa3bee6cdf22c5cd263b55619
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-07-06 11:24:15 -0300

    feat(types): add controller binding to container
    Added `controllers.person` to AdonisJS container types. This ensures type safety and improves developer experience when accessing the `PersonsController`.

--- types/person_bindings.ts
+++ types/person_bindings.ts
@@ -1,9 +1,11 @@
 import PersonRepository from '#modules/person/repositories/person_repository'
 import PersonService from '#modules/person/services/person_service'
+import PersonsController from '#modules/person/controllers/persons_controller'
 
 declare module '@adonisjs/core/types' {
   export interface ContainerBindings {
     'repositories.person': PersonRepository
     'services.person': PersonService
+    'controllers.person': PersonsController
   }
 }
