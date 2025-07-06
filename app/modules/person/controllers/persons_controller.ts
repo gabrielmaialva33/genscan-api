abab277033dbcc50b1eb2c79964e084213cfc84c
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-07-06 13:35:05 -0300

    feat(person): extend listing filters in PersonsController
    Enhanced `PersonsController.index` to support additional filters such as pagination, sorting, search, marital status, and birth date. Integrated `luxon` to handle ISO date parsing for the `birth_date` filter. These changes improve the flexibility and functionality of person listing.

--- app/modules/person/controllers/persons_controller.ts
+++ app/modules/person/controllers/persons_controller.ts
@@ -4,6 +4,7 @@ import type { HttpContext } from '@adonisjs/core/http'
 import { createPersonValidator } from '#modules/person/validators/create_person_validator'
 import CreatePersonService from '#modules/person/services/create-person/create_person_service'
 import ListPersonsService from '#modules/person/services/list-persons/list_persons_service'
+import { DateTime } from 'luxon'
 
 @inject()
 export default class PersonsController {
@@ -13,8 +14,26 @@ export default class PersonsController {
   ) {}
 
   async index({ request }: HttpContext) {
+    const page = request.input('page', 1)
+    const perPage = request.input('per_page', 10)
+    const sortBy = request.input('sort_by', 'id')
+    const direction = request.input('order', 'asc')
+    const search = request.input('search')
     const gender = request.input('gender')
-    const persons = await this.listPersonsService.run(gender)
+    const maritalStatus = request.input('marital_status')
+    const birthDate = request.input('birth_date')
+
+    const persons = await this.listPersonsService.run({
+      page,
+      perPage,
+      sortBy,
+      direction,
+      search,
+      gender,
+      maritalStatus,
+      birthDate: birthDate ? DateTime.fromISO(birthDate) : null,
+    })
+
     return persons
   }
 
