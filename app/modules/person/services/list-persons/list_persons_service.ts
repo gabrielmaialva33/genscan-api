3f32911ed0fed469813b6bf3c74d8f2f3402ec77
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-07-06 13:34:50 -0300

    feat(person): enhance person listing with advanced filters
    Added support for additional filters in `ListPersonsService`, including `search`, `maritalStatus`, and `birthDate`. Refactored the service to use a `paginate` method for improved query flexibility and implementation efficiency.

--- app/modules/person/services/list-persons/list_persons_service.ts
+++ app/modules/person/services/list-persons/list_persons_service.ts
@@ -1,11 +1,41 @@
 import { inject } from '@adonisjs/core'
 import PersonRepository from '#modules/person/repositories/person_repository'
+import { DateTime } from 'luxon'
+
+type PaginatePersonsRequest = {
+  page: number
+  perPage: number
+  sortBy: string
+  direction: 'asc' | 'desc'
+  search?: string
+  gender?: string
+  maritalStatus?: string
+  birthDate: DateTime | null
+}
 
 @inject()
 export default class ListPersonsService {
   constructor(private personRepository: PersonRepository) {}
 
-  async run() {
-    return this.personRepository.list()
+  async run(options: PaginatePersonsRequest) {
+    const { search, gender, maritalStatus, birthDate, ...paginateOptions } = options
+
+    return this.personRepository.paginate({
+      ...paginateOptions,
+      scopes: (scopes) => {
+        if (gender) {
+          scopes.byGender(gender)
+        }
+        if (search) {
+          scopes.bySearch(search)
+        }
+        if (maritalStatus) {
+          scopes.byMaritalStatus(maritalStatus)
+        }
+        if (birthDate) {
+          scopes.byBirthDate(birthDate)
+        }
+      },
+    })
   }
 }
